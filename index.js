ec19ba4ad10a82ba8fd9945a2176f47a05b49bbd
Edmond <edmond.santiago7@gmail.com>
2024-03-06 19:35:08 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordReversed = word.split("").reverse().join("");
+  if (word === wordReversed) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -7,14 +12,16 @@ function isPalindrome(word) {
 */
 
 /*
-  Add written explanation of your solution here
+  Splitting a string, reversing it then joining it together, gives me word in its reversed state.
+  Once I have this data, I can match the argument passed to the reversed word to see if it matches.
+  I then used an if statement to return the result true or false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("rotator"));
 
   console.log("");
 
